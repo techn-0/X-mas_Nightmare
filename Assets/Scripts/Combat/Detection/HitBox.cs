9019a53a2dd55061d2a520a7b75d5327374895e9
techn-0 <gnlrjswjd@naver.com>
2025-12-20 19:42:03 +0900

    Revert "근접공격 모션만 나가는 문제 해결"
    This reverts commit 067e156f2595328e7008fecfbad61b68de7a73bb.

--- Assets/Scripts/Combat/Detection/HitBox.cs
+++ Assets/Scripts/Combat/Detection/HitBox.cs
@@ -59,7 +59,8 @@ namespace Game.Combat
         public void Activate()
         {
             isActive = true;
-            Debug.Log($"[HitBox] {gameObject.name} ACTIVATED - ready to hit!");
+            gameObject.SetActive(true); // GameObject도 활성화
+            Debug.Log($"[HitBox] {gameObject.name} ACTIVATED - ready to hit! GameObject enabled.");
         }
         
         /// <summary>
@@ -68,7 +69,8 @@ namespace Game.Combat
         public void Deactivate()
         {
             isActive = false;
-            Debug.Log($"[HitBox] {gameObject.name} DEACTIVATED.");
+            gameObject.SetActive(false); // GameObject도 비활성화
+            Debug.Log($"[HitBox] {gameObject.name} DEACTIVATED - GameObject disabled.");
         }
         
         /// <summary>
